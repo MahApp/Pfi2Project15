5d70125e4bf3bf0ebe9f7d1418d0ae26c6bed705
Bankmongot <nilsson.viktor@live.se>
2015-03-16 13:04:02 +0100

    Added test class for text sidescroll animation.

---
+++ Pfi2Project15/src/trafikinfo/animation.java
@@ -0,0 +1,85 @@
+package trafikinfo;
+
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class animation {
+	
+	private void display() {
+        JFrame f = new JFrame("MarqueeTest");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        String s = "Här skrivs info ut, "
+        + "Vi kan göra den hur lång som heeeeeeeeeeeelst, "
+        + "Det är bara att kötta påååååå; ... "
+        + "bra va?";
+        MarqueePanel mp = new MarqueePanel(s, 32);
+        f.add(mp);
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        mp.start();
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new animation().display();
+            }
+        });
+    }
+}
+
+/** Side-scroll n characters of s. */
+class MarqueePanel extends JPanel implements ActionListener {
+
+    private static final int RATE = 12;
+    private final Timer timer = new Timer(1000 / RATE, this);
+    private final JLabel label = new JLabel();
+    private final String s;
+    private final int n;
+    private int index;
+
+    public MarqueePanel(String s, int n) {
+        if (s == null || n < 1) {
+            throw new IllegalArgumentException("Null string or n < 1");
+        }
+        StringBuilder sb = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+            sb.append(' ');
+        }
+        this.s = sb + s + sb;
+        this.n = n;
+        label.setFont(new Font("Serif", Font.ITALIC, 36));
+        label.setText(sb.toString());
+        this.add(label);
+    }
+
+    public void start() {
+        timer.start();
+    }
+
+    public void stop() {
+        timer.stop();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        index++;
+        if (index > s.length() - n) {
+            index = 0;
+        }
+        label.setText(s.substring(index, index + n));
+    }
+}
+	
+	
+
