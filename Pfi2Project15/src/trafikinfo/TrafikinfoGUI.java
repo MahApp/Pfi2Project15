f58604e8b9c477904bce8829e63509513677ba01
EmergencyBattle <sugarcubehere@gmail.com>
2015-03-16 13:20:40 +0100

    hmmmm

---
+++ Pfi2Project15/src/trafikinfo/TrafikinfoGUI.java
@@ -0,0 +1,80 @@
+package trafikinfo;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.UIManager;
+
+import se.mah.k3.pfi2.project.main.controller.ModuleInterface;
+
+public class TrafikinfoGUI extends JFrame implements ModuleInterface{
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TrafikinfoGUI frame = new TrafikinfoGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TrafikinfoGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1080, 1920);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(193,0,43));
+		panel.setBounds(0, 80, 1064, 80);
+		contentPane.add(panel);
+	}
+	
+	@Override
+	public int getExpectedPriority() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPreferdNumberOfRows() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMinNumberOfRows() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean showNumberOfRows(int start, int end) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void repaintPanel() {
+		// TODO Auto-generated method stub
+		
+	}
+}
