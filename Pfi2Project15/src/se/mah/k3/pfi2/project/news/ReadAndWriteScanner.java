84f90cb847590118613f13feb08f63a4a2ed8308
JoelHenning <JoelHenning@10.1.21.94>
2015-03-16 16:26:27 +0100

    Testat lite med att läsa in strängar

--- Pfi2Project15/src/se/mah/k3/pfi2/project/news/ReadAndWriteScanner.java
+++ Pfi2Project15/src/se/mah/k3/pfi2/project/news/ReadAndWriteScanner.java
@@ -15,12 +15,48 @@ public class ReadAndWriteScanner {
 		try {
 			URL mah = new URL("http://www.hamnaratt.com/veckans-lunch");
 			Scanner s = new Scanner(mah.openStream());
-			while (s.hasNext("Måndag")){
+			while (s.hasNext()){
 				String string = s.nextLine();
-//				System.out.println(string);
-				System.out.println(s.findInLine("Kalendern"));
-				System.out.println(s.nextLine());
-				s.close();
+//				System.out.println(s.findInLine("Kalendern")); // Kanske kan användas
+				if (string.contains("Veckans Lunch v")){
+					int i = string.indexOf("Veckans");
+					int i2 = string.indexOf("</h1>");
+					String resultat = string.substring(i,  i2);
+					System.out.println(resultat);
+				}
+				
+				String s2 = s.nextLine();
+				if (s2.contains("ndag")){
+					System.out.println("Måndag");
+				}
+				
+				String dagens = s.nextLine();
+				if (dagens.contains("Husman")){
+					//System.out.println(dagens);
+					int d = dagens.indexOf("Husman");
+					int d2 = dagens.indexOf("stomp");
+					String dagensResultat = dagens.substring(d, d2);
+					System.out.println(dagensResultat);
+				}
+				
+				String veg = s.nextLine();
+				if (veg.contains("VeckoVeg")){
+					//System.out.println(veg);
+					int v = veg.indexOf("VeckoVeg");
+					int v2 = veg.indexOf("</strong>");
+					String vegResultat = veg.substring(v,  v2);
+					System.out.println(vegResultat);
+				}
+				
+				String kapet = s.nextLine();
+				if (kapet.contains("Kapet")){
+					//System.out.println(kapet);
+					int k = kapet.indexOf("Kapet");
+					int k2 = kapet.indexOf("</strong>");
+					String kapetResultat = kapet.substring(k,  k2);
+					System.out.println(kapetResultat);
+				}
+				
 			}
 			s.close();
 		} catch (Exception e) {
