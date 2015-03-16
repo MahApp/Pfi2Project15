3874f741c632bab42ce58aa979a70c902a7c0570
Therealnaxos <nils.lindgren.92@gmail.com>
2015-03-16 15:07:01 +0100

    Ny testclass

---
+++ Pfi2Project15/src/trafikinfo/TestClassForCode.java
@@ -0,0 +1,19 @@
+package trafikinfo;
+
+import java.util.Calendar;
+
+
+public class TestClassForCode {
+	public static void main(String[] args) {
+		String searchURL = Constants.getURL("80000","81216",20);
+		System.out.println(searchURL);
+		System.out.println("// Results when searching:");
+		
+		Journeys journeys = Parser.getJourneys(searchURL);
+		for (Journey journey : journeys.getJourneys()) {
+			String effect = journey.getDepDeviationAffect();
+			System.out.print(effect+ "\n");
+		} 
+		
+	}
+}
