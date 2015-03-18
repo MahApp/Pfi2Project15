7450ea5d92a66aa3cb961610996650660870a722
Alrik <Alrikhe@192.168.1.64>
2015-03-18 02:42:31 +0100

    Xml first gen parse

---
+++ Pfi2Project15/src/se/mah/k3/pfi2/project/kronox/Post.java
@@ -0,0 +1,22 @@
+package se.mah.k3.pfi2.project.kronox;
+
+//
+public class Post{
+	private float x,y,vx,vy,ax,ay; // for animation
+public Cell[] cell;
+void Post(){
+	
+	
+	
+	
+	
+}
+public void display(){
+	
+	
+	
+	
+} 
+
+
+}
