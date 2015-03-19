5ab52aa103c7aa2ef5bc7b6c33459f9794529e3f
Therealnaxos <nils.lindgren.92@gmail.com>
2015-03-19 20:22:01 +0100

    Fix in resolution in GUI

--- Pfi2Project15/src/trafikinfo/TrafikinfoGUI.java
+++ Pfi2Project15/src/trafikinfo/TrafikinfoGUI.java
@@ -41,7 +41,7 @@ public class TrafikinfoGUI extends JFrame implements ModuleInterface{
 	 */
 	public TrafikinfoGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1080, 1920);
+		setBounds(100, 100, 1080, 80);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
