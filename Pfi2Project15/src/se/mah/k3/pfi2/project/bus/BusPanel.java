38a45b326691cd9903aaf20b0992ca817559f080
Alexander Hansson <Alexander.Hansson@hotmail.com>
2015-03-19 14:16:37 +0100

    Busstiderna uppdateras nu varje minut. Designjobb återstår.

--- Pfi2Project15/src/se/mah/k3/pfi2/project/bus/BusPanel.java
+++ Pfi2Project15/src/se/mah/k3/pfi2/project/bus/BusPanel.java
@@ -3,6 +3,8 @@ package se.mah.k3.pfi2.project.bus;
 import javax.swing.JPanel;
 
 import se.mah.k3.pfi2.project.main.controller.ModuleInterface;
+import se.mah.k3lara.skaneAPI.control.Constants;
+import se.mah.k3lara.skaneAPI.model.Journey;
 import se.mah.k3lara.skaneAPI.model.Journeys;
 import se.mah.k3lara.skaneAPI.model.Line;
 import se.mah.k3lara.skaneAPI.model.Lines;
@@ -12,6 +14,7 @@ import se.mah.k3lara.skaneAPI.xmlparser.Parser;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Timer;
 
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
@@ -27,7 +30,7 @@ public class BusPanel extends JPanel implements ModuleInterface{
 	/**
 	 * 
 	 */
-	
+
 	private static final long serialVersionUID = 1L;
 	public JTextArea Line;
 	public ArrayList<Station> searchStations = new ArrayList<Station>();
@@ -45,25 +48,25 @@ public class BusPanel extends JPanel implements ModuleInterface{
 		lineThread.start();
 
 		setBackground(Color.LIGHT_GRAY);
-		setLayout(new MigLayout("", "[112px][112px][112px,grow][112px,grow][112px,grow]", "[150px][150px,grow]"));
-		
+		setLayout(new MigLayout("", "[][112px][112px][112px,grow][112px,grow][112px,grow]", "[150px][150px,grow][150px,grow][150px,grow][150px,grow]"));
+
 		JLabel lblLinje = new JLabel("Linje");
 		lblLinje.setFont(new Font("Tahoma", Font.BOLD, 24));
 		lblLinje.setForeground(new Color(0, 0, 0));
 		lblLinje.setVerticalAlignment(SwingConstants.TOP);
-		add(lblLinje, "flowx,cell 0 0,alignx center,aligny top");
+		add(lblLinje, "flowx,cell 1 0,alignx center,aligny top");
 
 		JLabel lblNewLabel = new JLabel("L\u00E4ge");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
 		lblNewLabel.setForeground(new Color(0, 0, 0));
 		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
-		add(lblNewLabel, "cell 3 0,alignx center,growy");
+		add(lblNewLabel, "cell 4 0,alignx center,growy");
 
 		JLabel lblAvgr = new JLabel("Avg\u00E5ng");
 		lblAvgr.setFont(new Font("Tahoma", Font.BOLD, 24));
 		lblAvgr.setForeground(new Color(0, 0, 0));
 		lblAvgr.setVerticalAlignment(SwingConstants.TOP);
-		add(lblAvgr, "cell 4 0,grow");
+		add(lblAvgr, "cell 5 0,grow");
 
 		Line = new JTextArea();
 		Line.setRows(2);
@@ -71,7 +74,7 @@ public class BusPanel extends JPanel implements ModuleInterface{
 		Line.setAutoscrolls(false);
 		Line.setFont(new Font("Tahoma", Font.BOLD, 14));
 		Line.setEditable(false);
-		add(Line, "flowx,cell 0 1,alignx center,growy");
+		add(Line, "flowx,cell 1 1 1 4,alignx center,growy");
 
 		Destination = new JTextArea();
 		Destination.setEditable(false);
@@ -79,7 +82,7 @@ public class BusPanel extends JPanel implements ModuleInterface{
 		Destination.setFont(new Font("Tahoma", Font.BOLD, 14));
 		Destination.setRows(2);
 		Destination.setAutoscrolls(false);
-		add(Destination, "cell 1 1 2 1,grow");
+		add(Destination, "cell 2 1 2 4,grow");
 
 		Stop = new JTextArea();
 		Stop.setEditable(false);
@@ -87,7 +90,7 @@ public class BusPanel extends JPanel implements ModuleInterface{
 		Stop.setFont(new Font("Tahoma", Font.BOLD, 14));
 		Stop.setRows(2);
 		Stop.setAutoscrolls(false);
-		add(Stop, "cell 3 1,alignx center,growy");
+		add(Stop, "cell 4 1 1 4,alignx center,growy");
 
 		Departure = new JTextArea();
 		Departure.setEditable(false);
@@ -96,13 +99,13 @@ public class BusPanel extends JPanel implements ModuleInterface{
 		Departure.setRows(2);
 		Departure.setForeground(new Color(0, 0, 0));
 		Departure.setAutoscrolls(false);
-		add(Departure, "cell 4 1,alignx center,growy");
+		add(Departure, "cell 5 1 1 4,alignx center,growy");
 
 		JLabel lblDestination = new JLabel("Destination");
 		lblDestination.setFont(new Font("Tahoma", Font.BOLD, 24));
 		lblDestination.setForeground(new Color(0, 0, 0));
 		lblDestination.setVerticalAlignment(SwingConstants.TOP);
-		add(lblDestination, "cell 1 0 2 1,grow");
+		add(lblDestination, "cell 2 0 2 1,grow");
 	}
 
 	public class LineThread extends Thread{
@@ -114,88 +117,62 @@ public class BusPanel extends JPanel implements ModuleInterface{
 		}
 
 		public void run() {
-			Lines lines = Parser.getStationResults(new Station("80046"));
-			int results = 0;
-			// ï¿½r det inte bï¿½ttre att anvï¿½nda String.format %02d ist? 
-			for (Line l : lines.getLines()) {
-				results++;
-				
-				Calendar cal = Calendar.getInstance();
+			try {
+				while (true) {
+					String searchURL = Constants.getURL("80046","80000",4);
+					System.out.println(searchURL);
+					Journeys journeys = Parser.getJourneys(searchURL); 
 
-				int H1 = cal.get(Calendar.HOUR);
-				int M1 = cal.get(Calendar.MINUTE);
-				int H = l.getDepTime().get(Calendar.HOUR_OF_DAY );
-				int M = l.getDepTime().get(Calendar.MINUTE );
+					Thread.sleep(60000);
+					Line.setText("");
+					Destination.setText("");
+					Stop.setText("");
+					Departure.setText("");
+					for (Journey journey : journeys.getJourneys()) {
 
-				long difference = H - H1;	
-				long difference1 = M - M1;
+						int HJ= journey.getDepDateTime().get(Calendar.HOUR_OF_DAY);
+						int MJ = journey.getDepDateTime().get(Calendar.MINUTE);
+						String time=(String.format("%02d",HJ)+":"+(String.format("%02d",MJ)));
+						String depTime = journey.getTimeToDeparture()+journey.getDepTimeDeviation();
 
-				if (difference1 < 0) {
-					difference1 =  M1;
+						Line.append(journey.getLineOnFirstJourney() + "\n");
+						Destination.append(journey.getTowards()+"\n");
+						Stop.append(journey.getStopPoint() + "\n");
+						System.out.println(depTime);
+
+						if(Integer.valueOf(depTime) > 10 && Integer.valueOf(depTime) > 0) {
+							Departure.append(time + "\n");
+						} else {
+							Departure.append(journey.getTimeToDeparture()+journey.getDepTimeDeviation() + " min \n");
+						}
+					}
 				}
-
-				if (difference <= 0) {
-					System.out.println("Den här bussen gick för " + (-difference-difference) + " minuter sedan");
-				} else {
-					Line.append(l.getLine() + "\n");
-					Destination.append(l.getTowards()+"\n");
-					Stop.append(l.getStopPoint() + "\n");
-					Departure.append(String.format("%02d", difference) +":" + String.format("%02d", difference1) + "\n");
-					System.out.println("En buss kommer om " + difference + " minuter");
-				}
-				if (results == 4) {
-					break;
-				}
-			}
+			} catch (Exception ex){
+			} start();
 		}
 	}
 
-	////				String minutes = "";
-	////				
-	////				if (String.valueOf(l.getDepTime().get(Calendar.MINUTE)).length() == 1) {
-	////					minutes = "0" + String.valueOf(l.getDepTime().get(Calendar.MINUTE));
-	////				} else {
-	////					minutes = String.valueOf(l.getDepTime().get(Calendar.MINUTE));
-	////				}
-	//				
-	//				Line.append(l.getLine() + "\n");
-	//				Destination.setText(l.getTowards() + "\n");
-	////				if (Parser.hours == false) {
-	////				Departure.append(l.getTimeToDeparture() + " min \n");
-	////				} else {
-	//					Departure.setText(String.valueOf(l.getDepTime().get(Calendar.HOUR_OF_DAY)+":"+l.getDepTime().get(Calendar.MINUTE)));
-	//					Departure.setText("\n");
-	////				}
-	//			}		
-	//		}
-	//	}
-
 	@Override
 	public int getExpectedPriority() {
-		// Prioritet 1 fï¿½r testsyfte
 		return 1;
 	}
 
 	@Override
 	public int getPreferdNumberOfRows() {
-		// TODO Auto-generated method stub
 		return 4;
 	}
 
 	@Override
 	public int getMinNumberOfRows() {
-		// TODO Auto-generated method stub
 		return 1;
 	}
 
 	@Override
 	public boolean showNumberOfRows(int start, int end) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void repaintPanel() {
-		// TODO Auto-generated method stub
 	}
 }
