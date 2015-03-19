9584f1e07a84d69b22809b343d813f198803a14e
JoelHenning <JoelHenning@192.168.0.11>
2015-03-19 14:39:08 +0100

    Läsning av RSS i ScannerKalender-klass

--- Pfi2Project15/src/se/mah/k3/pfi2/project/news/ScannerKalender.java
+++ Pfi2Project15/src/se/mah/k3/pfi2/project/news/ScannerKalender.java
@@ -12,18 +12,30 @@ public class ScannerKalender {
 
 	public void readAndWriteEvents(){
 		try {
-			URL mahKalender = new URL("http://www.mah.se/Nyheter/Kalender");
+			URL mahKalender = new URL("http://www.mah.se/Nyheter/RSS/Kalender-fran-Malmo-hogskola/");
 			Scanner s = new Scanner(mahKalender.openStream());
 			while (s.hasNext()) {
 				String string = s.nextLine();
-				if (string.contains("mars")){
-				//	System.out.println(string);
-					int i = string.indexOf("mars");
-					int i2 = string.indexOf("</h2>");
-					String resultat = string.substring(i,  i2);
-					System.out.println(resultat);
+			//	System.out.println(string);
+				String s2 = s.nextLine();
+				if (s2.contains("Öppet hus")){
+					System.out.println(s2);
+//					int i = s2.indexOf("Tid:");
+//					int i2 = s2.indexOf("</strong>");
+//					String resultat = s2.substring(i, i2);
+//					int k = s2.indexOf("12:");
+//					int k2 = s2.indexOf(":00 ");
+//					String tid = s2.substring(k, k2);
+//					int l = s2.indexOf("17:");
+//					int l2 = s2.indexOf(":00<br />");
+//					String tidSlut = s2.substring(l,  l2);
+//					int p = s2.indexOf("Orkanen");
+//					int p2 = s2.indexOf("<br /><strong>Mål");
+//					String plats = s2.substring(p, p2);
+//					System.out.println(tid + "-" + tidSlut + " " + "Öppet hus" + " " + plats);
 				}
-			}
+		
+				}
 			s.close();
 		}
 		catch (Exception e) {
