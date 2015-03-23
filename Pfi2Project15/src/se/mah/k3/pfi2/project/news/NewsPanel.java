64ba799798137c7bc41dc3800e9b2147e3f04481
emeli_000 <emeli_000@Emelie>
2015-03-23 15:25:16 +0100

    Lunch GUI Jpanel

--- Pfi2Project15/src/se/mah/k3/pfi2/project/news/NewsPanel.java
+++ Pfi2Project15/src/se/mah/k3/pfi2/project/news/NewsPanel.java
@@ -41,18 +41,29 @@ public class NewsPanel extends JPanel implements ModuleInterface{
 	}
 	public NewsPanel() {
 		setForeground(UIManager.getColor("scrollbar"));
-		setBorder(new LineBorder(Color.WHITE, 5));
+		setBorder(null);
 		setBackground(new Color(240, 241, 241));
 		setLayout(null);
 		
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(823, 19, 202, 196);
+		add(lblNewLabel);
+		lblNewLabel.setIcon(new ImageIcon(NewsPanel.class.getResource("/Images/salad.jpg")));
+		
+		JPanel RAM = new JPanel();
+		RAM.setBounds(816, 15, 217, 205);
+		add(RAM);
+		RAM.setBackground(new Color(134,188,37));
+		
 		JPanel panel2 = new JPanel();
-		panel2.setBounds(17, 18, 662, 241);
+		panel2.setBounds(17, 11, 1047, 218);
 		panel2.setBackground(new Color(227, 230, 229));
 		add(panel2);
 		panel2.setLayout(null);
 		
 		JPanel Rubrik = new JPanel();
-		Rubrik.setBounds(24, 0, 218, 50);
+		Rubrik.setBounds(116, 11, 218, 50);
 		Rubrik.setBackground(new Color(134,188,37));
 		panel2.add(Rubrik);
 		
@@ -65,21 +76,10 @@ public class NewsPanel extends JPanel implements ModuleInterface{
 				
 				Meny.setFont(new Font("Futura", Font.PLAIN, 20));
 				Meny.setText("Här kommer menyn stå!");
-				Meny.setBounds(24, 57, 381, 163);
+				Meny.setBounds(98, 70, 381, 163);
 				Meny.setBackground(new Color(227, 230, 229));
 				Meny.setForeground(new Color(97, 101, 109));
 				panel2.add(Meny);
-				
-				
-				JLabel lblNewLabel = new JLabel("New label");
-				lblNewLabel.setIcon(new ImageIcon(NewsPanel.class.getResource("/Images/salad.jpg")));
-				lblNewLabel.setBounds(438, 19, 202, 196);
-				panel2.add(lblNewLabel);
-				
-				JPanel RAM = new JPanel();
-				RAM.setBounds(435, 16, 208, 204);
-				panel2.add(RAM);
-				RAM.setBackground(new Color(134,188,37));
 		//panel.setBackground(new Color(134, 188, 37));
 
 	}
