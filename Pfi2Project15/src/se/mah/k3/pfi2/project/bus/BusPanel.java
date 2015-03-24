7d165863d6897ffdc7d9b16e2e38c8ff971bb139
Alexander Hansson <Alexander.Hansson@hotmail.com>
2015-03-25 00:40:20 +0100

    Tråden uppdateras nu utan att rita ut fler JTables

--- Pfi2Project15/src/se/mah/k3/pfi2/project/bus/BusPanel.java
+++ Pfi2Project15/src/se/mah/k3/pfi2/project/bus/BusPanel.java
@@ -32,44 +32,26 @@ public class BusPanel extends JPanel implements ModuleInterface {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private JTable tableTitle;
+	private JTable tableDepartures;
+
 	private String line = "1";
 	private String destination = "";
 	private String stop = "";
 	private String departure = "";
 
+	private int noOfUpdates = 0;
+	private int updateInterval = 2000;
+	private int results = 4;
+
 	public ArrayList<Station> searchStations = new ArrayList<Station>();
 	public Journeys journeys;
 	private Parser parser = new Parser();
 
-	private int noOfUpdates = 0;
-	private int updateInterval = 2000;
-	private int results = 4;
-	private JTable tableTitle;
-	private JTable tableDepartures;
-
-	//HÄMTA OCH SKRIV UT AVGÅNGAR
-	public void Departures() {
-		String searchURL = Constants.getURL("80046", "80000", results);
-		Journeys journeys = Parser.getJourneys(searchURL);
-
-		for (Journey journey : journeys.getJourneys()) {
-			int HJ = journey.getDepDateTime().get(Calendar.HOUR_OF_DAY);
-			int MJ = journey.getDepDateTime().get(Calendar.MINUTE);
-			String time = (String.format("%02d", HJ) + ":" + (String.format("%02d", MJ)));
-			String depTime = journey.getTimeToDeparture() + journey.getDepTimeDeviation();
-
-			line = (journey.getLineOnFirstJourney());
-			destination = (journey.getTowards());
-			stop = (journey.getStopPoint());
-
-			if (Integer.valueOf(depTime) > 10 && Integer.valueOf(depTime) > 0) {
-				departure = (time);
-			} else {
-				departure = (journey.getTimeToDeparture() + journey.getDepTimeDeviation() + " min");
-			}
-		}
-
-		//SKAPA JTABLE FÖR TITLAR
+	/**
+	 *Skapa JTable för titlar.
+	 */
+	private void titleTable() {
 		tableTitle = new JTable();
 		tableTitle.setForeground(Color.BLACK);
 		tableTitle.setBorder(new LineBorder(Color.GRAY, 3, true));
@@ -87,10 +69,12 @@ public class BusPanel extends JPanel implements ModuleInterface {
 						"Line", "Destination", "Stop", "Departure"
 				}
 				));
-		add(tableTitle, "cell 1 0,grow");
+	}
 
-		//SKAPA JTABLE FÖR AVGÅNGAR
-		
+	/**
+	 *Skapa JTable för avgångar.
+	 */
+	private void departuresTable() {
 		tableDepartures = new JTable();
 		tableDepartures.setFont(new Font("Futura LT", Font.PLAIN, 42));
 		tableDepartures.setRowSelectionAllowed(false);
@@ -108,12 +92,11 @@ public class BusPanel extends JPanel implements ModuleInterface {
 						"Line", "Destination", "Stop", "Departure"
 				}
 				));
-		add(tableDepartures, "cell 0 1 3 1,grow");
-
-		setRowHeights();
 	}
 
-	//BESTÄM RADHÖJD
+	/**
+	 *Bestäm radhöjd på JTables.
+	 */
 	private void setRowHeights()
 	{
 		try
@@ -139,25 +122,60 @@ public class BusPanel extends JPanel implements ModuleInterface {
 	}
 
 	/**
+	 *Hämta och skriv ut avgångar.
+	 */
+	public void Departures() {
+		String searchURL = Constants.getURL("80046", "80000", results);
+		Journeys journeys = Parser.getJourneys(searchURL);
+
+		for (Journey journey : journeys.getJourneys()) {
+			int HJ = journey.getDepDateTime().get(Calendar.HOUR_OF_DAY);
+			int MJ = journey.getDepDateTime().get(Calendar.MINUTE);
+			String time = (String.format("%02d", HJ) + ":" + (String.format("%02d", MJ)));
+			String depTime = journey.getTimeToDeparture() + journey.getDepTimeDeviation();
+
+			line = (journey.getLineOnFirstJourney());
+			destination = (journey.getTowards());
+			stop = (journey.getStopPoint());
+
+			if (Integer.valueOf(depTime) > 10 && Integer.valueOf(depTime) > 0) {
+				departure = (time);
+			} else {
+				departure = (journey.getTimeToDeparture() + journey.getDepTimeDeviation() + " min");
+			}
+		}
+		titleTable();
+		departuresTable();
+		setRowHeights();
+	}
+
+
+
+	/**
 	 * Create the panel.
 	 */
 	public BusPanel() {
 		setBorder(new LineBorder(Color.GRAY, 5, true));
 		setBackground(Color.WHITE);
-		setLayout(new MigLayout("insets 0", "[1%:1%:1%][grow][1%:1%:1%,grow]", "[grow][grow]"));
+		setLayout(new MigLayout("insets 0", "[1%:n][grow][::1%,grow]", "[grow][grow]"));
 
 		//BYTA FÄRG PÅ VARANNAN RAD
 		UIDefaults defaults = UIManager.getLookAndFeelDefaults();
 		if (defaults.get("Table.alternateRowColor") == null)
 			defaults.put("Table.alternateRowColor", new Color(240, 240, 240));
 
-		//SKRIV UT AVGÅNGAR EN GÅNG OCH STARTA TRÅD
+		//SKRIV UT AVGÅNGAR EN GÅNG, RITA JTABLES OCH STARTA TRÅD
 		Thread lineThread = new BusPanel.LineThread(parser);
 		Departures();
+		add(tableDepartures, "cell 0 1 3 1,grow");
+		add(tableTitle, "cell 1 0,grow");
 		System.out.println("START\n" + "UPPDATERINGSINTERVALL: " + updateInterval/1000 + "s\n");
 		lineThread.start();
 	}
 
+	/**
+	 * Uppdatera avgångar.
+	 */
 	public class LineThread extends Thread {
 		private Parser parser;
 
@@ -168,7 +186,7 @@ public class BusPanel extends JPanel implements ModuleInterface {
 		public void run() {			
 			try {
 				while (true) {
-					//Departures();
+					Departures();
 					noOfUpdates++;
 					System.out.println("UPPDATERING " + noOfUpdates + "\n");
 					Thread.sleep(updateInterval);
