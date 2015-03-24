77d152cebb113c0da6a1cb930557c57a7f3a75cf
Bankmongot <nilsson.viktor@live.se>
2015-03-24 20:25:53 +0100

    Module now viewable in Setup.

--- Pfi2Project15/src/se/mah/k3/pfi2/project/traficinfo/view/TrafficInfo.java
+++ Pfi2Project15/src/se/mah/k3/pfi2/project/traficinfo/view/TrafficInfo.java
@@ -3,97 +3,41 @@ package se.mah.k3.pfi2.project.traficinfo.view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.EventQueue;
+import java.awt.Dimension;
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
-import javax.swing.UIManager;
 
 import se.mah.k3.pfi2.project.main.controller.ModuleInterface;
 
-import javax.swing.JTextArea;
+public class TrafficInfo extends JPanel implements ModuleInterface{
 
-import java.awt.Font;
-
-public class TrafficInfo extends JFrame implements ModuleInterface{
-
-	private JPanel contentPane;
+	
 	TrafficInfo g = this;
 
+	
+	
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TrafficInfo frame = new TrafficInfo();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	} 
-
-	/**
-	 * Create the frame.
+	 * Create the panel.
 	 */
 	public TrafficInfo() {
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0, 0, 1080, 160);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		contentPane.setBackground(new Color(193,0,43));
+		setPreferredSize(new Dimension(1080,160));
+		setMinimumSize(new Dimension(1080,160));
+		setBackground(new Color(193,0,43));
+		setLayout(null);
 		
-		
-		
-		/* ********************IF WE USE A SCROLLING STRING****************************** */
-		// Placement on screen to be edited depending on what we decide in priority discussions
-		/*JPanel panelString = new JPanel();
-		panelString.setBackground(new Color(193,0,43));
-		panelString.setBounds(0, 160, 1080, 80);
-		contentPane.add(panelString);
-		panelString.setLayout(null);*/
-		
-		/*JLabel lblNewLabel = new JLabel("TRAFIKST\u00D6RNING:");
-		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-		lblNewLabel.setDisplayedMnemonicIndex(0);
-		lblNewLabel.setVerticalAlignment(SwingConstants.BOTTOM);
-		lblNewLabel.setFont(new Font("Futura Std Heavy", Font.BOLD, 23));
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setBounds(10, -5, 290, 46);
-		add(lblNewLabel); */
-		
-		/*JLabel left310 = new JLabel("New label");
-		left310.setIcon(new ImageIcon(TrafficInfo.class.getResource("/resources/left_part_310x80.PNG")));
-		left310.setBounds(-300, 0, 310, 80);
-		add(left310);*/
-		
-		/*JLabel middle580 = new JLabel("New label");
-		middle580.setIcon(new ImageIcon(TrafficInfo.class.getResource("/resources/middle_part_580x80.PNG")));
-		middle580.setBounds(310, 0, 580, 80);
-		add(middle580); */
-		
+
 		JLabel right190 = new JLabel("New label");
+		right190.setBounds(0, 0, 1080, 160);
 		right190.setInheritsPopupMenu(false);
 		right190.setIcon(new ImageIcon(TrafficInfo.class.getResource("/resources/warningsign.png")));
-		right190.setBounds(0, 0, 1080, 160);
 		add(right190);
-		
-		
 		
 		Thread tx = new gThread(g);
 		tx.start();	
+		
 	}
 	
 	@Override
