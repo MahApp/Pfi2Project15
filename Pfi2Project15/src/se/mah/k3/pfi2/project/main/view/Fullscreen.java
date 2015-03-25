a9dccdd1a6b16657fa87da32c6a098aac111774c
JoelHenning <JoelHenning@10.1.21.94>
2015-03-25 12:35:05 +0100

    fixat så info om lunch skrivs ut i lunchpanel

--- Pfi2Project15/src/se/mah/k3/pfi2/project/main/view/Fullscreen.java
+++ Pfi2Project15/src/se/mah/k3/pfi2/project/main/view/Fullscreen.java
@@ -22,6 +22,7 @@ import se.mah.k3.pfi2.project.bus.BusPanel;
 import se.mah.k3.pfi2.project.dummypanel.DummyPanel;
 import se.mah.k3.pfi2.project.kronox.KronoxPanel;
 import se.mah.k3.pfi2.project.main.controller.ModuleInterface;
+import se.mah.k3.pfi2.project.news.LunchPanel;
 import se.mah.k3.pfi2.project.news.NewsPanel;
 
 public class Fullscreen extends JFrame implements KeyEventDispatcher {
@@ -56,6 +57,7 @@ public class Fullscreen extends JFrame implements KeyEventDispatcher {
 	private void setupPanels() {
 		//moduleList.add(new KronoxPanel());
 		moduleList.add(new NewsPanel());
+		moduleList.add(new LunchPanel());
 		moduleList.add(new FillEmptySpace());
 		int yPlace = 0;
 		for (ModuleInterface moduleInterface : moduleList) {
--- Pfi2Project15/src/se/mah/k3/pfi2/project/news/LunchPanel.java
+++ Pfi2Project15/src/se/mah/k3/pfi2/project/news/LunchPanel.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.net.URL;
+import java.util.Scanner;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -12,10 +14,14 @@ import javax.swing.JTextArea;
 import javax.swing.UIManager;
 
 import se.mah.k3.pfi2.project.main.controller.ModuleInterface;
+
 import javax.swing.border.LineBorder;
 
 public class LunchPanel extends JPanel implements ModuleInterface{
-
+	
+	public JLabel dagensLabel;
+	public JLabel kapet;
+	public JLabel veg;
 	
 	/**
 	 * Create the panel.
@@ -49,23 +55,23 @@ public class LunchPanel extends JPanel implements ModuleInterface{
 					lblNews.setForeground(Color.WHITE);
 					Rubrik.add(lblNews);
 					
-					JLabel Dagens = new JLabel("New label");
-					Dagens.setFont(new Font("Futura", Font.PLAIN, 20));
-					Dagens.setBounds(17, 75, 536, 29);
-					panel2.add(Dagens);
+					dagensLabel = new JLabel("");
+					dagensLabel.setFont(new Font("Futura", Font.PLAIN, 20));
+					dagensLabel.setBounds(17, 75, 536, 29);
+					panel2.add(dagensLabel);
 					
-					JLabel Kapet = new JLabel("New label");
-					Kapet.setFont(new Font("Futura", Font.PLAIN, 20));
-					Kapet.setBounds(16, 116, 535, 33);
-					panel2.add(Kapet);
+					kapet = new JLabel("");
+					kapet.setFont(new Font("Futura", Font.PLAIN, 20));
+					kapet.setBounds(16, 116, 535, 33);
+					panel2.add(kapet);
 					
-					JLabel lblNewLabel_2 = new JLabel("New label");
-					lblNewLabel_2.setFont(new Font("Futura", Font.PLAIN, 20));
-					lblNewLabel_2.setBounds(16, 157, 529, 38);
-					panel2.add(lblNewLabel_2);
+					veg = new JLabel("");
+					veg.setFont(new Font("Futura", Font.PLAIN, 20));
+					veg.setBounds(16, 157, 529, 38);
+					panel2.add(veg);
 			//panel.setBackground(new Color(134, 188, 37));
 					
-					
+					readAndWriteMonday();
 
 		}
 
@@ -99,5 +105,75 @@ public class LunchPanel extends JPanel implements ModuleInterface{
 		public void repaintPanel() {
 			// TODO Auto-generated method stub
 			
+		}
+		
+		public void readAndWriteMonday(){
+			try {
+				URL freda49 = new URL("http://www.freda49.se/lunch-malmo.html");
+				Scanner sc = new Scanner(freda49.openStream());
+				while (sc.hasNext()) {
+					String fullText = sc.nextLine();
+					
+//					int menuStart = fullText.indexOf("LUNCHMENY");   //fr�n "lunchmeny"
+//					int menuEnd = fullText.indexOf("L=inneh");       //till "L=inneh�ller laktos"
+//					String fullMenu = fullText.substring(menuStart, menuEnd);
+//					
+//					int mondayStart = fullMenu.indexOf("ndag:");
+//					int mondayEnd = fullMenu.indexOf("TISDAG");
+//					String monday = fullMenu.substring(mondayStart, mondayEnd);
+					
+					String dagens = fullText;
+					if (dagens.contains("Husman")){	
+						int dagensStartRead = dagens.indexOf("Husman");
+						int dagensEndRead = dagens.indexOf("lsa:");
+						String dagensResultat = dagens.substring(dagensStartRead, dagensEndRead);
+						
+						int cleanStart = dagensResultat.indexOf("Husman:");
+						int cleanEnd = dagensResultat.indexOf("</span>");
+						String cleanResultat = dagensResultat.substring(cleanStart, cleanEnd);
+						
+						
+						System.out.println(cleanResultat);
+						dagensLabel.setText(cleanResultat);
+						
+					}
+					
+					String halsa = fullText;
+					if (halsa.contains("lsa:")){
+						int halsaStartRead = halsa.indexOf("lsa:");
+						int halsaEndRead = halsa.indexOf("Vegetarisk");
+						String halsaResultat = halsa.substring(halsaStartRead,  halsaEndRead);
+						
+						int cleanStart = halsaResultat.indexOf("lsa:");
+						int cleanEnd = halsaResultat.indexOf("</span>");
+						String cleanResultat = halsaResultat.substring(cleanStart, cleanEnd);
+								
+								
+						System.out.println("Ha" + cleanResultat);
+						veg.setText("Ha" + cleanResultat);
+					}
+					
+					String veg = fullText;
+					if (veg.contains("Vegetarisk:")){ 
+						int vegStartRead = veg.indexOf("Vegetarisk");    
+						int vegEndRead = veg.indexOf(">TISDAG<");
+						String vegResultat = veg.substring(vegStartRead,  vegEndRead); //tar in all text inom taggarna "Vegetarisk" och 
+																					   //">TISDAG<"
+						
+						int cleanStart = vegResultat.indexOf("Vegetarisk");
+						int cleanEnd = vegResultat.indexOf("</span>");
+						String cleanResultat = vegResultat.substring(cleanStart, cleanEnd); //tar in all text fr�n b�rjan av vegResultat och 
+																						    // </span> inom vegresultat
+						
+						System.out.println(cleanResultat);   //skriver ut vegResultat utan HTML-taggar
+						kapet.setText(cleanResultat);
+					}
+					
+				}
+				sc.close();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 }
