12f191da8f97592a7199b7880da797752788fe2c
Alexander Hansson <Alexander.Hansson@hotmail.com>
2015-03-26 22:11:39 +0100

    lade till kod för SetupBus

--- Pfi2Project15/src/se/mah/k3/pfi2/project/main/view/Fullscreen.java
+++ Pfi2Project15/src/se/mah/k3/pfi2/project/main/view/Fullscreen.java
@@ -3,6 +3,7 @@ package se.mah.k3.pfi2.project.main.view;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridBagConstraints;
@@ -12,17 +13,31 @@ import java.awt.KeyEventDispatcher;
 import java.awt.KeyboardFocusManager;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Enumeration;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.FontUIResource;
 
 import se.mah.k3.pfi2.project.bus.BusPanel;
 import se.mah.k3.pfi2.project.dummypanel.DummyPanel;
+import se.mah.k3.pfi2.project.kronox.CanvasInJframe;
 import se.mah.k3.pfi2.project.kronox.KronoxPanel;
 import se.mah.k3.pfi2.project.main.controller.ModuleInterface;
+import se.mah.k3.pfi2.project.main.controller.Setup;
+import se.mah.k3.pfi2.project.news.LunchPanel;
 import se.mah.k3.pfi2.project.news.NewsPanel;
+import se.mah.k3.pfi2.project.social.SocialPanel;
+import se.mah.k3.pfi2.project.timeweather.TimePanel;
+import se.mah.k3.pfi2.project.timeweather.WeatherPanel;
+import se.mah.k3.pfi2.project.timeweather.WeatherPanelBig;
+import se.mah.k3.pfi2.project.traficinfo.view.TrafficInfo;
+
+
 
 public class Fullscreen extends JFrame implements KeyEventDispatcher {
 	/**
@@ -33,16 +48,19 @@ public class Fullscreen extends JFrame implements KeyEventDispatcher {
 	private boolean inFullScreenMode = false;
 	private int PrevX = 100 ,PrevY = 100 ,PrevWidth = 480,PrevHeight = 640;
 	public static ArrayList<ModuleInterface> moduleList = new ArrayList<ModuleInterface>();
+	private boolean debug=true;
 	
 	
 	/**
 	 * Create the frame.
+	 * @param mode 
 	 */
-	public Fullscreen() {
+	public Fullscreen(String mode) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(249,179,0));
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
@@ -50,15 +68,94 @@ public class Fullscreen extends JFrame implements KeyEventDispatcher {
 		contentPane.setLayout(gbl_contentPane);
 		KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager(); //Listen to keyboard
         manager.addKeyEventDispatcher(this);
-        setupPanels();
+        //Apply a font
+        InputStream is = null;
+		Font font = null;
+		is = Setup.class.getResourceAsStream("FuturaLT.ttf");
+		//is = Setup.class.getResourceAsStream("Lobster-Regular.ttf");
+		 try {
+			 GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			if (font==null){font = Font.createFont(Font.TRUETYPE_FONT, is);}
+			ge.registerFont(font);
+			setUIFont(new FontUIResource(font.deriveFont(Font.TRUETYPE_FONT, 12)));
+		   } catch (Exception e) {
+			   System.out.println("Couldn't load or register font");
+		   }
+		 if (debug){
+			 System.out.println("All installed fontnames");
+			 GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			 Font[] f = ge.getAllFonts();
+			 for (Font font2 : f) {
+				System.out.println(font2.getFontName());
+			}
+			 Font f3 = new Font("Futura LT Medium", Font.PLAIN, 20);
+				System.out.println("NAME: "+f3.getName());
+				System.out.println("NAME: "+f3.getFontName());
+				
+				System.out.println("NAME: -1 is ok "+f3.canDisplayUpTo("Abcdˆ‰Â≈ƒ÷"));
+			 System.out.println("========================");
+		 }
+        setupPanels(mode);
+		
 	}
+	public static void setUIFont(FontUIResource f) {
+        Enumeration keys = UIManager.getDefaults().keys();
+        while (keys.hasMoreElements()) {
+            Object key = keys.nextElement();
+            Object value = UIManager.get(key);
+            if (value instanceof FontUIResource) {
+                FontUIResource orig = (FontUIResource) value;
+                Font font = new Font(f.getFontName(), orig.getStyle(), f.getSize());
+                UIManager.put(key, new FontUIResource(font));
+            }
+        }
+    }
 	
-	private void setupPanels() {
+	private void setupPanels(String mode) {
+		//Comment and uncomment here to show your panel
+		//Add the panels not yet merged
+		//Add more modes if needed to show your module so it shows best
+		//Also add a setupfile to match in main.controller
+		//Show as many modules as possible in a setup to avoid restarting many times
+		if (mode.equals("basic")){
+			moduleList.add(new TimePanel());
+			moduleList.add(new KronoxPanel());
+			moduleList.add(new FillEmptySpace());
+		}else if (mode.equals("klockKronoxWeatherBusandSocial")){
+			moduleList.add(new TimePanel());
+			moduleList.add(new KronoxPanel());
+			moduleList.add(new WeatherPanel());
+			moduleList.add(new BusPanel());
+			moduleList.add(new SocialPanel());
+			moduleList.add(new FillEmptySpace());
+		}else if (mode.equals("klockKronoxWeatherBus")){
+			moduleList.add(new TimePanel());
+			moduleList.add(new KronoxPanel());
+			moduleList.add(new WeatherPanel());
+	//		moduleList.add(new WeatherPanelBig());
+			//moduleList.add(new NewsPanel());
+			moduleList.add(new BusPanel());
+			//moduleList.add(new SocialPanel());
+			//moduleList.add(new TrafficInfo());
+			moduleList.add(new FillEmptySpace());
+			//moduleList.add(new KronoxPanel());
+			//moduleList.add(new NewsPanel());
+			//moduleList.add(new LunchPanel());
+			//moduleList.add(new FillEmptySpace());
+		}
+		moduleList.add(new TimePanel());
+		moduleList.add(new KronoxPanel());
+	//	moduleList.add(new WeatherPanel());
+		//moduleList.add(new WeatherPanelBig());
+		//moduleList.add(new NewsPanel());
+	//	moduleList.add(new BusPanel());
+	//	moduleList.add(new SocialPanel());
+	//	moduleList.add(new TrafficInfo());
+	//	moduleList.add(new FillEmptySpace());
 //		moduleList.add(new KronoxPanel());
-//		moduleList.add(new DummyPanel());
 //		moduleList.add(new NewsPanel());
+//		moduleList.add(new LunchPanel());
 		moduleList.add(new FillEmptySpace());
-		moduleList.add(new BusPanel());
 		int yPlace = 0;
 		for (ModuleInterface moduleInterface : moduleList) {
 			GridBagConstraints cons = new GridBagConstraints();
@@ -77,7 +174,7 @@ public class Fullscreen extends JFrame implements KeyEventDispatcher {
 			}
 			contentPane.add((Component) moduleInterface, cons);
 		}
-		setFullscreen(false);
+		setFullscreen(true);
 		
 	}
 
